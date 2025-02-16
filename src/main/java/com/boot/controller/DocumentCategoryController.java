15bf3b21dced502cdfefc2e6b0329e9ee65dec95
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-02-16 22:13:09 +0530

    Rename Document category

---
+++ src/main/java/com/boot/controller/DocumentCategoryController.java
@@ -0,0 +1,11 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocumentCategoryController {
+
+	
+	
+
+}
---
+++ src/main/java/com/boot/serviceimpl/DocumentCategoryServiceImplementation.java
@@ -0,0 +1,5 @@
+package com.boot.serviceimpl;
+
+public class DocumentCategoryServiceImplementation {
+
+}
