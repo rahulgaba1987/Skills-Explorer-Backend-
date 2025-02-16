af31a8d42123333a11bb4d7507691f7d342f79d7
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-02-16 21:55:44 +0530

    created All Layers of Skills Explorer

---
+++ src/main/java/com/boot/common/CommonConstants.java
@@ -0,0 +1,5 @@
+package com.boot.common;
+
+public class CommonConstants {
+
+}
---
+++ src/main/java/com/boot/exception/GlobalException.java
@@ -0,0 +1,8 @@
+package com.boot.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+
+}
